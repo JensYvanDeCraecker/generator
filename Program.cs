4fa6f9145be6bec16324f987fdb62f7b8ec55027
Jens Yvan De Craecker <jens.de.craecker@outlook.be>
2022-07-12 22:03:14 +0000

    Add stars in generator

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
-﻿var numbers = GenerateNumbers();
+﻿var numbers = new List<int>();
+var stars = new List<int>();
 var seed = (DateTime.Now - new DateTime(1998, 06, 30)).Ticks;
 var rnd = new Random((int)seed);
 var maxRows = 9;
-var maxNumbers = 8;
+var maxNumbers = 50;
+var maxStars = 12;
+var maxNumbersPerRow = 6;
+var maxStarsPerRow = 2;
 
 for (var currentRow = 1; currentRow <= maxRows; currentRow++)
 {
+    var rowNumbers = GenerateRow(numbers, maxNumbersPerRow, maxNumbers, rnd).OrderBy(v => v);
+    var rowStars = GenerateRow(stars, maxStarsPerRow, maxStars, rnd).OrderBy(v => v);
+    Console.WriteLine($"{currentRow}: {string.Join(" - ", rowNumbers.Select(v => v.ToString()).Concat(rowStars.Select(v => "*" + v)))}");
+}
+
+static IEnumerable<int> GenerateRow(List<int> numbers, int maxRowNumbers, int maxNumbers, Random rnd)
+{
     var row = new List<int>();
-    var numbersToGenerate = maxNumbers;
-    if (numbers.Count < maxNumbers)
+    var numbersToGenerate = maxRowNumbers;
+    if (numbers.Count < maxRowNumbers)
     {
         numbersToGenerate -= numbers.Count;
         row.AddRange(numbers);
-        numbers = GenerateNumbers(numbers);
+        numbers.Clear();
+        numbers.AddRange(GenerateNumbers(maxNumbers, numbers));
     }
     for (var currentNumber = 1; currentNumber <= numbersToGenerate; currentNumber++)
     {
@@ -20,8 +32,7 @@ for (var currentRow = 1; currentRow <= maxRows; currentRow++)
         row.Add(numbers[index]);
         numbers.RemoveAt(index);
     }
-
-    Console.WriteLine($"{currentRow}: {string.Join(" - ", row.OrderBy(v => v))}");
+    return row;
 }
 
-static IList<int> GenerateNumbers(IEnumerable<int>? except = null) => Enumerable.Range(1, 50).Except(except ?? Enumerable.Empty<int>()).ToList();
+static IEnumerable<int> GenerateNumbers(int maxNumbers, IEnumerable<int> except) => Enumerable.Range(1, maxNumbers).Except(except);
